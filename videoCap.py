7d19592b9d518418f8efe2668e1e737798b49b53
ArnoVD <arno.vandiest@hotmail.com>
2020-12-27 14:38:00 +0100

    controller output

--- videoCap.py
+++ videoCap.py
@@ -2,5 +2,7 @@ import cv2
 import time
 cap= cv2.VideoCapture(0)
 while(True):
+    time.sleep(2)
     _,img = cap.read()
     cv2.imwrite('Documents\\'+ str(time.time())+ '.png',img)
+    print("picture taken")
