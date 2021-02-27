fc3875d82ab89e954550a1938637dc27494d3d28
ArnoVD <arno.vandiest@hotmail.com>
2021-02-27 12:41:54 +0100

    test forward backward

--- joystick.py
+++ joystick.py
@@ -22,13 +22,13 @@ class MyController(Controller):
 
     def on_L3_up(self):
         print("Forward!")
-        self.bw.speed = self.motor_speed
-        self.bw.forward()
+        #self.bw.speed = self.motor_speed
+        #self.bw.forward()
 
     def on_L3_down(self):
         print("Backward!")
-        self.bw.speed = self.motor_speed
-        self.bw.backward()
+        #self.bw.speed = self.motor_speed
+        #self.bw.backward()
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
