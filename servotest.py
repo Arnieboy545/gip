701e47a03d54302e5b3cd2f29f4580a618572f8c
ArnoVD <arno.vandiest@hotmail.com>
2021-04-09 13:44:24 +0200

    test forward backward

--- servotest.py
+++ servotest.py
@@ -2,6 +2,11 @@
 from picar import front_wheels, back_wheels
 from picar.SunFounder_PCA9685 import Servo
 import picar
+from time import sleep
+import cv2
+import numpy as np
+import picar
+import os
 
     pan_servo = Servo.Servo(1)
     picar.setup()
