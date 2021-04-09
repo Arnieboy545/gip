35b7973ef5b6a7ff8bde54d310c1137509c92747
ArnoVD <arno.vandiest@hotmail.com>
2021-04-09 13:46:51 +0200

    test forward backward

--- servotest.py
+++ servotest.py
@@ -8,12 +8,12 @@ import numpy as np
 import picar
 import os
 
-    pan_servo = Servo.Servo(1)
-    picar.setup()
+pan_servo = Servo.Servo(1)
+picar.setup()
 
-    fw.offset = 0
-    pan_servo.offset = 10
+fw.offset = 0
+pan_servo.offset = 10
 
-    fw.turn(45)
-    pan_servo.write(45)
+fw.turn(45)
+pan_servo.write(45)
 
